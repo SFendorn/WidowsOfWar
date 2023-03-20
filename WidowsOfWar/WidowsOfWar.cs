45806ea682aa9364ff136678ec87a57a0f0059d7
Fendorn <mail+git@fendorn.de>
2023-03-20 13:59:54 +0100

    Prepare C# solution with Harmony support for advanced modding

---
+++ WidowsOfWar/WidowsOfWar.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using TaleWorlds.MountAndBlade;
+
+namespace WidowsOfWar
+{
+    public class WidowsOfWar : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            Harmony harmony = new Harmony("WidowsOfWarHarmony");
+            harmony.PatchAll(Assembly.GetAssembly(GetType()));
+        }
+    }
+}
